5b5aaf5a4cc5a739eb23acd669aa0eb6f3ca8cde
EC2 Default User <ec2-user@ip-172-31-60-95.us-west-2.compute.internal>
2019-05-01 23:57:40 +0000

    both get photo routes are set up

---
+++ redis/redis.js
@@ -0,0 +1,18 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const redis = require('redis');
+
+const client = redis.createClient({
+  password: process.env.REDISPASS,
+});
+
+client.on('connect', () => {
+  console.log('connected to redis!')
+})
+
+client.on('error', () => {
+  console.log('error connecting to redis')
+})
+
+
+module.exports = client;
--- server/index.js
+++ server/index.js
@@ -1,3 +1,58 @@
 const dotenv = require('dotenv');
 dotenv.config();
-console.log(`${process.env.PORT}`)
+
+const redisDB = require('/home/ec2-user/redis/redis.js');
+const request = require('request-promise')
+const http = require('http');
+
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+console.log(req.url.substr(1))
+  if(req.method === 'GET') {
+    if(req.url === '/restNames') {
+      redisDB.get('popular', (err, result) => {
+        if(result) {
+            res.writeHead(200, {
+              'Content-Type': 'application/json'
+            })
+            res.end(result);
+        } else {
+          let options = {
+            uri: 'http://ec2-54-218-12-68.us-west-2.compute.amazonaws.com:3004/restNames'
+          }
+          request(options).then(popular => {
+	    redisDB.set('popular', JSON.stringify(popular));
+            res.writeHead(200, {
+   	      'Content-Type': 'application/json'
+            })
+            res.end(JSON.stringify(popular));
+          }).catch(err => console.log(err));  
+        }
+      })
+    } else if(isNaN(req.url.substr(1)) !== true) {
+      let id = req.url.substr(1);
+        console.log(id, 'THIS IS THR ID') 
+     redisDB.get(id, (err, photoResult) => {
+        if(photoResult) {
+          res.writeHead(200, {
+            'Content-Type': 'application/json'
+          })
+	  res.end(photoResult);
+        } else {
+        let options = {
+        uri: `http://ec2-54-218-12-68.us-west-2.compute.amazonaws.com:3004/${id}`
+        }
+      request(options).then(photos => {
+        redisDB.set(id, JSON.stringify(photos))
+        res.writeHead(200,{
+            'Content-Type': 'application/json'
+           })
+            res.end(photos);
+          }).catch(err => console.log(err, 'THIS IS TFROM THE CATCH BLCOK'))
+        }
+      })
+    }
+  }
+  
+}).listen(PORT, () => console.log(`SERVER LISTENING ON ${PORT}`))
